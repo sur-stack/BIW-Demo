b71093876d36ab743848b8a54f0e8ef4bbcdeca5
abc <abc@example.com>
2020-08-18 13:48:22 +0530

    INIT

---
+++ Sign in/js/main.js
@@ -0,0 +1,41 @@
+var left_cover = document.getElementById("left-cover");
+var left_form = document.getElementById("left-form");
+
+var right_cover = document.getElementById("right-cover");
+var right_form = document.getElementById("right-form");
+
+function switchSignup() {
+    right_form.classList.add("fade-in-element");
+    left_cover.classList.add("fade-in-element");
+
+    left_form.classList.add("form-hide");
+    left_cover.classList.remove("cover-hide");
+    right_form.classList.remove("form-hide");
+    right_cover.classList.add("cover-hide");
+}
+
+
+$(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+    
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+    });
---
+++ javascript/dashboard.js
@@ -0,0 +1,23 @@
+// This is for Demo page
+$(document).ready(function(){
+    $('.demo').click(function(){
+        $('.demos').load('demo.html');
+    });
+});
+
+// This is for aboutUs page
+
+$(document).ready(function(){
+    $('.about').click(function(){
+        $('.abouts').load('about.html');
+    });
+});
+
+
+// This is for curriculum page
+
+$(document).ready(function(){
+    $('.curriculum').click(function(){
+        $('.curriculums').load('curriculum.html');
+    });
+});
